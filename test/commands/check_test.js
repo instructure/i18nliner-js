94f8b132a7c10ceaa1eff6a97fd40c0b3d25a483
Artsiom Bolhar <laizerart@yandex.ru>
2022-02-01 10:31:12 +0100

    Tests extension to test ts files

--- test/commands/check_test.js
+++ test/commands/check_test.js
@@ -6,13 +6,26 @@ import {assert} from "chai";
 
 describe('Check', function() {
   describe(".run", function() {
-    it("should find errors", function() {
-      I18nliner.set('basePath', "test/fixtures", function() {
+    it("should find errors in js files", function() {
+      I18nliner.set('basePath', "test/fixtures/i18n_js", function() {
         var checker = new Check({silent: true});
         checker.run();
         assert.deepEqual(
           checker.translations.translations,
           {"welcome_name_4c6ebc3a": 'welcome, %{name}'}
+        );
+        assert.equal(checker.errors.length, 1);
+        assert.match(checker.errors[0], /^invalid signature/);
+      });
+    });
+
+    it("should find errors in ts files", function() {
+      I18nliner.set('basePath', "test/fixtures/i18n_ts", function() {
+        var checker = new Check({silent: true});
+        checker.run();
+        assert.deepEqual(
+          checker.translations.translations,
+          {"welcome_fname_45c9b6bf": 'welcome, %{fname}'}
         );
         assert.equal(checker.errors.length, 1);
         assert.match(checker.errors[0], /^invalid signature/);
---
+++ test/fixtures/i18n_ts/invalid.ts
@@ -0,0 +1,4 @@
+var I18n = { t: (str: string, vars?: any) => str };
+var fname = 'fname';
+
+I18n.t('welcome, ' + fname);
